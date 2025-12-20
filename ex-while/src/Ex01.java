3756fd497afd20358499c5d57d581fa01966cb99
dbuzatto <diogobuzatto16@gmail.com>
2025-12-20 18:13:11 -0300

    fix: adiciona parâmetros String[] args ao main

--- ex-while/src/Ex01.java
+++ ex-while/src/Ex01.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ex01 {
-    public static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Exercício 01");
 
