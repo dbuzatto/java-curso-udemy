60e3ca26e01f79332619caaabd789788f1542445
dbuzatto <diogobuzatto16@gmail.com>
2025-12-23 20:53:18 -0300

    feat: segundo ex, employee

---
+++ ex-employee-poo/src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Employee employee = new Employee();
+
+        System.out.print("Name: ");
+        employee.name = sc.nextLine();
+
+        System.out.print("Gross Salary: ");
+        employee.grossSalary = sc.nextDouble();
+
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.printf("Employee :" + employee);
+
+        System.out.print("\nWhich percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.printf("Updated data: " + employee);
+
+        sc.close();
+    }
+}
