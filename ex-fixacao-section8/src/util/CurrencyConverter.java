78993dffd20b854f6b40a29fae15608cf679333d
dbuzatto <diogobuzatto16@gmail.com>
2025-12-25 16:35:09 -0300

    feat: ex de fixação Section 8

---
+++ ex-fixacao-section8/src/util/CurrencyConverter.java
@@ -0,0 +1,10 @@
+package util;
+
+public class CurrencyConverter {
+
+    public static double dollarToReais(double price, double amountDollars){
+
+        return price * amountDollars * 1.06;
+
+    }
+}
