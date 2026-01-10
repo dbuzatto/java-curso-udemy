3d24bd18fa6d6cd5c0e995ed7f44c35b0a3058c3
dbuzatto <diogobuzatto16@gmail.com>
2026-01-09 21:58:39 -0300

    feat: adicionar exercício feito junto ao professor sobre enum

---
+++ enum-section12/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
+
+        sc.close();
+    }
+}
