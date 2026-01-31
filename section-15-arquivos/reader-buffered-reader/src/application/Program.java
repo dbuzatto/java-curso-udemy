359faf4ff6d1fccf60fcf8afe3a2f986bc06cff2
dbuzatto <diogobuzatto16@gmail.com>
2026-01-31 16:17:59 -0300

    chore: melhorar qualidade de código com try-with-resources

--- section-15-arquivos/reader-buffered-reader/src/application/Program.java
+++ section-15-arquivos/reader-buffered-reader/src/application/Program.java
@@ -8,13 +8,8 @@ public class Program {
     public static void main(String[] args) {
 
         String path = "/Users/diogobuzatto/Downloads/teste.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null){
@@ -24,19 +19,6 @@ public class Program {
         }
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
